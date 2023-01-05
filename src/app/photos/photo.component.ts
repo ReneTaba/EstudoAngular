0ac14f337d914466a3d65e66b07f98e65dcad6e9
Rene Paulo Taba da Silva <renetaba@gmail.com>
2023-01-05 16:55:14 -0300

    Usando um NgFor

--- src/app/photos/photo.component.ts
+++ src/app/photos/photo.component.ts
@@ -6,6 +6,6 @@ import { Component, Input } from '@angular/core';
 })
 export class PhotoComponent{
   title = 'Este he o Titulo';
-  @Input() discription = '';
+  @Input() description = '';
   @Input() url = '';
 }
