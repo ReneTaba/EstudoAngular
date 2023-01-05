3cd066421db03a073a75c2a4165220618f28a27c
Rene Paulo Taba da Silva <renetaba@gmail.com>
2023-01-05 16:13:51 -0300

    Usando o NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { PhotoComponent } from './photo.component';
+import { PhotosModule } from './photos/photos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PhotoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        PhotosModule
+    ]
 })
 export class AppModule { }
