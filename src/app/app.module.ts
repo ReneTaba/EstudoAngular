d9cf9b3d8562d4fff49db03366a0be6e12405325
Rene Paulo Taba da Silva <renetaba@gmail.com>
2023-01-10 09:48:29 -0300

    Usando API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { PhotosModule } from './photos/photos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
---
+++ src/app/photos/photo.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Photo } from "./photo";
+
+const API = 'http://localhost:3000';
+
+// Incluido o Provider do PhotoService no root
+// assim todos os modulos podem acessar a mesma instancia do compenente
+@Injectable({ providedIn: 'root' })
+export class PhotoService {
+
+    // colocamos o private para poder usar na classe
+    constructor(private http: HttpClient) {}
+
+    // Se nao colocar o String da erro
+    listFromUser(userName: string) {
+        return this.http
+            .get<Photo[]>(API + '/flavio/photos');
+    }
+
+}
